d2448f060057d60d84024467c74d19d0f058b89d
Kenny Wong <kennyw728@gmail.com>
2015-08-15 21:11:47 -0700

    filtering

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,6 +5,14 @@ angular.module('leagueItemSetsApp')
         
         RiotService.Champions.Get().then(function(result){
             $scope.champions = result.data.data;
-            console.log($scope.champions);
         });
+        
+        $scope.filter = function(filter){
+            angular.forEach($scope.champions, function(championDetail, championName){
+                championDetail.hide = true;
+                if(championName.toLowerCase().indexOf(filter) > -1){
+                    championDetail.hide = false;
+                }
+            })
+        };
     });
