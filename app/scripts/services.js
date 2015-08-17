8f4eaec5155b384154bba6f305f05c34d7142e1f
Nate Frueh <ntfrueh@gmail.com>
2015-08-16 23:43:53 -0400

    Addition of Champion Detail Page
    Basic page created for specific champions (currently displays champion
    name, icon, and title via new Champion service). Added contact page.

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -3,14 +3,19 @@ angular
         .service('RiotService', ['$http', function ($http) {
             
             var baseURL = 'https://global.api.pvp.net/';
-            var APIKey = '&api_key=ab9ef72b-ea32-4508-bcf7-51ab654189e5';
-            
-            var Champions = {
+            var APIKey = '&api_key=6140bf70-7719-45c3-804d-300dc1c8f55f';
+
+        var Champions = {
                 Get: function(){ return $http.get(baseURL + 'api/lol/static-data/na/v1.2/champion?champData=image' + APIKey) }
             };
 
+            var Champion = {
+                Get: function(champID){ return $http.get(baseURL + 'api/lol/static-data/na/v1.2/champion/' + champID +'?champData=all' + APIKey)}
+            };
+
             return {
-                Champions: Champions
+                Champions: Champions,
+                Champion: Champion
             }
         }
     ]);
