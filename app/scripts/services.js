ab4dce7b808714ab70a1a00897bba99ce0f02ece
Kenny Wong <kennyw728@gmail.com>
2015-08-17 18:58:01 -0700

    removed api key from services.js

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,8 +1,8 @@
 angular.module('leagueItemSetsApp')
-    .service('RiotService', ['$http', function ($http) {
+    .service('RiotService', ['$http', 'apiKey', function ($http, apiKey) {
 
             var baseURL = 'https://global.api.pvp.net/';
-            var APIKey = 'api_key=ab9ef72b-ea32-4508-bcf7-51ab654189e5';
+            var APIKey = apiKey.GetKey();
 
             var Champions = {
                 Get: function () { return $http.get(baseURL + 'api/lol/static-data/na/v1.2/champion?champData=image&' + APIKey); },
